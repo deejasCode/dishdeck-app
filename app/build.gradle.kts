318dfc869d1694540c55234496fc7590e7852aa4
deejasCode <141955693+deejasCode@users.noreply.github.com>
2026-06-07 20:37:41 +0200

    Add RecipeListScreen with categories and sample data

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,12 +40,14 @@ android {
 dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.compose.material)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.material3)
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
@@ -55,4 +57,5 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.tooling)
     implementation("androidx.navigation:navigation-compose:2.9.8")
     implementation(libs.coil.compose)
+    implementation("androidx.compose.material:material-icons-extended:1.7.8")
 }
